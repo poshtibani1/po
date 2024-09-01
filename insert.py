664784e739e70df6ff4210d6100b9ee5adeb3c1f
poshtibani1 <poshtibanipagein@gmail.com>
2024-09-01 11:01:40 +0000

    h

--- insert.py
+++ insert.py
@@ -9,25 +9,25 @@ rs.trust_env = False
 
 print("[!] Changes to all .txt files will be applied from \"insert.txt\"")
 
-if True:
+try:
     insert_file = open("./insert.txt", "r", encoding="utf-8")
     insert_lines = insert_file.readlines()
     files = [os.path.join("./", f) for f in os.listdir("./") if os.path.isfile(os.path.join("./", f)) and f.lower().endswith(".txt") and f.lower() != "insert.txt"]
     for file in files:
-        if True:
+        try:
             f = open(file, "r+", encoding="utf-8")
             lines = f.readlines()
             f.close()
             for i in range(len(insert_lines), 0, -1):
                 if insert_lines[i-1].strip().lower().startswith("http://") or insert_lines[i-1].strip().lower().startswith("https://"):
-                    if True:
+                    try:
                         request = rs.get(insert_lines[i-1].strip(), timeout=30, allow_redirects=True, verify=False)
                         rt = request.text
                         rt_list = rt.split("\n")
                         for r in range(len(rt_list), 0, -1):
                             if rt_list[r-1].strip() != "":
                                 lines.insert(1, rt_list[r-1].strip())
-                    elif False:
+                    except:
                         print(f"[X] Error link: {insert_lines[i-1].strip()}")
                 elif insert_lines[i-1].strip() == "":
                     pass
@@ -37,8 +37,8 @@ if True:
             f.write("\n".join([f.strip() for f in lines]))
             f.close()
             print(f"[ok] Inserted: {os.path.basename(file)}")
-        elif False:
+        except:
             print(f"[X] Error: {os.path.basename(file)}")
     insert_file.close()
-elif False:
+except:
     print("[X] There's no \"insert.txt\" file")
